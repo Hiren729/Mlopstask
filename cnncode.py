6210bcc098fd654edbaf189ea18cad500b8fe47a
Hirendra Ruprela <hirenruprela@gmail.com>
2020-05-30 15:33:25 +0530

    final commit

--- cnncode.py
+++ cnncode.py
@@ -63,10 +63,6 @@ model.add(Conv2D(20, (5, 5), input_shape = input_shape))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
-#model.add(Conv2D(50, (5, 5) ))
-#model.add(Activation("relu"))
-#model.add(MaxPooling2D(pool_size = (2, 2),strides = (2, 2)))
-
 
 # Fully connected layers (w/ RELU)
 model.add(Flatten())
